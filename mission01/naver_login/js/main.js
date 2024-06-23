94678c0989f00e53c29ab99a82df4d68347bdbfe
zidoopal <yuzzzin@gmail.com>
2024-06-23 18:14:36 +0900

    Feat: 로그인 유효성 검사 및 이벤트 처리 구현 완료
    - 정규 표현식을 사용하여 이메일 및 비밀번호 유효성 검사 추가
    - 이벤트 위임을 통한 실시간 유효성 검사 구현
    - 로그인 버튼 클릭 시 최종 유효성 검사 및 사용자 인증 확인 추가
    - 로그인 성공 시 환영 페이지로 리다이렉션
    - 잘못된 입력 및 인증 실패 시 오류 메시지 표시

--- mission01/naver_login/js/main.js
+++ mission01/naver_login/js/main.js
@@ -1,36 +1,49 @@
+import { emailReg, pwReg } from '../lib/utils/authValidation.js';
 
 const user = {
-  id:'asd@naver.com',
-  pw:'spdlqj123!@'
-}
+  id: 'asd@naver.com',
+  pw: 'spdlqj123!@',
+};
 
-/*
+const loginForm = document.querySelector('.login-form');
+const emailInput = document.querySelector('#userEmail');
+const passwordInput = document.querySelector('#userPassword');
+const loginButton = document.querySelector('.btn-login');
 
-1. email 정규표현식을 사용한 validation
-2. pw 정규표현식을 사용한 validation
-3. 상태 변수 관리
-4. 로그인 버튼을 클릭시 조건처리
+let isEmailValid = false;
+let isPasswordValid = false;
 
-*/
+const validateInput = (input, regex) => {
+  const isValid = regex(input.value);
+  isValid
+    ? input.classList.remove('is--invalid')
+    : input.classList.add('is--invalid');
+  return isValid;
+};
 
-function emailReg(text){
-  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+loginForm.addEventListener('input', (e) => {
+  const target = e.target;
 
-  return re.test(String(text).toLowerCase())
-}
+  if (target.id === 'userEmail') {
+    isEmailValid = validateInput(target, emailReg);
+  } else if (target.id === 'userPassword') {
+    isPasswordValid = validateInput(target, pwReg);
+  }
+});
 
-function pwReg(text){
-  const re = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^*+=-]).{6,16}$/;
-  return re.test(String(text).toLowerCase());
-}
+loginButton.addEventListener('click', (event) => {
+  event.preventDefault();
 
+  const emailValid = validateInput(emailInput, emailReg);
+  const passwordValid = validateInput(passwordInput, pwReg);
 
-
-
-
-
-
-
-
-
-
+  if (emailValid && passwordValid) {
+    if (emailInput.value === user.id && passwordInput.value === user.pw) {
+      window.location.href = 'welcome.html';
+    } else {
+      alert('아이디 또는 비밀번호가 일치하지 않습니다.');
+    }
+  } else {
+    alert('아이디 또는 비밀번호를 올바르게 입력해 주세요.');
+  }
+});
