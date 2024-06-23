822242de6dafbb661e1113b5ac33104d58bd851d
zidoopal <yuzzzin@gmail.com>
2024-06-23 18:12:46 +0900

    Feat: 이메일, 비밀번호 validation 함수 분리
    - emailReg, pwReg 함수 utils 폴더로 분리

---
+++ mission01/naver_login/lib/utils/authValidation.js
@@ -0,0 +1,11 @@
+export function emailReg(text) {
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  return re.test(String(text).toLowerCase());
+}
+
+export function pwReg(text) {
+  const re = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^*+=-]).{6,16}$/;
+  return re.test(String(text).toLowerCase());
+}
