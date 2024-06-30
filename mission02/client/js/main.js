c4af0cdf4d19d954deee42f02b62fd987bef4989
zidoopal2@gmail.com <zidoopal2@gmail.com>
2024-06-30 19:01:07 +0900

    Feat: 이미지&&배경 변경 기능 구현 완료

--- mission02/client/js/main.js
+++ mission02/client/js/main.js
@@ -1,24 +1,37 @@
+import { data } from './data.js';
+import { setBgColor, setImage, setNameText } from '../lib/utils/index.js';
 
 /* 
-
 1. 클릭 이벤트 활성화
 2. nav 클릭시 배경 색상 변경
 3. 이미지 변경
 4. 텍스트 변경
 5. 함수 분리
 
+
+이벤트 처리 방식을 사용하여 클릭 이벤트를 걸어주세요.
+    1. 이벤트 위임
+    2. 반복문
 */
 
+const nav = document.querySelector('nav');
+const poster = document.querySelector('.visual div img');
+const nickName = document.querySelector('.nickName');
 
+function handleClick(e) {
+  const targetLi = e.target.closest('li');
+  const liList = [...nav.querySelectorAll('li')];
 
+  if (!targetLi) return;
 
+  const index = targetLi.dataset.index - 1;
+  const selectedData = data[index];
 
+  liList.forEach((li) => li.classList.remove('is-active'));
+  targetLi.classList.add('is-active');
 
-
-
-
-
-
-
-
-
+  setBgColor(selectedData.color);
+  setImage(poster, selectedData);
+  setNameText(nickName, selectedData.name);
+}
+nav.addEventListener('click', handleClick);
